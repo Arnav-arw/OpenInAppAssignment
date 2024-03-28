e9b0880725a25a7092295565873441d6d66275af
Arnav Singhal <arnavsinghal.developer@gmail.com>
2024-03-28 17:01:57 +0530

    Initial Commit

---
+++ OpenInAppAssignment/OpenInAppAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenInAppAssignmentApp.swift
+//  OpenInAppAssignment
+//
+//  Created by Arnav Singhal on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenInAppAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
